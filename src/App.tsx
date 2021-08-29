9567007fce1b36042331ef121d355988c6250b1b
igor.alyoshin <igor.alyoshin@isoftbet.com>
2021-08-29 12:10:26 +0300

    Update: added handleClick and moveElevator functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,133 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (<div className="root"/>  );
-    /*<div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>*/
+interface State {
+  currentFloor: number;
+  upQueue: Set<number>;
+  downQueue: Set<number>;
+  moveDirection: MoveDirection
+}
+
+enum MoveDirection {
+  UP = "Up",
+  DOWN = "Down",
+  STANDING = "Standing"
+}
+
+class Elevator extends React.Component<{}, State> {
+
+  floorNumbers: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  constructor(props: {}) {
+    super(props);
+
+    this.state = {
+      currentFloor: this.floorNumbers[0],
+      upQueue: new Set(),
+      downQueue: new Set(),
+      moveDirection: MoveDirection.STANDING
+    };
+
+  }
+
+  private moveElevator(): void {
+    let delay = 1000;
+    
+    this.setState((state) => {
+
+      let { moveDirection, upQueue, downQueue, currentFloor } = state;
+      
+      if (upQueue.has(currentFloor) || downQueue.has(currentFloor)) {
+        upQueue = new Set(upQueue);
+        downQueue = new Set(downQueue);
+
+        upQueue.delete(currentFloor);
+        downQueue.delete(currentFloor);
+
+        if (!upQueue.size && !downQueue.size) {
+          moveDirection = MoveDirection.STANDING;
+        }
+        if (!upQueue.size && downQueue.size) {
+          moveDirection = MoveDirection.DOWN;
+        }
+        if (upQueue.size && !downQueue.size) {
+          moveDirection = MoveDirection.UP;
+        }
+
+        delay += 3000
+      }
+      
+      return {
+        upQueue,
+        downQueue,
+        moveDirection
+      };
+    }, () => {
+      if (this.state.moveDirection !== MoveDirection.STANDING) {
+        setTimeout(() => {
+          this.setState((state) => {
+            let floor = state.currentFloor;
+            floor += state.moveDirection === MoveDirection.UP ? 1 : -1;
+
+            return {
+              currentFloor: floor,
+            }
+          });
+          this.moveElevator();
+        }, delay)
+      }
+    });
+  }
+  
+  handleClick(floor: number): void {
+    this.setState((state) => {
+      let {moveDirection, upQueue, downQueue} = state;
+
+      if (floor > state.currentFloor) {
+        upQueue = new Set(upQueue).add(floor);
+        if (state.moveDirection === MoveDirection.STANDING) {
+          moveDirection = MoveDirection.UP;
+        }
+      }
+
+      if (floor < state.currentFloor) {
+        downQueue = new Set(downQueue).add(floor);
+        if (state.moveDirection === MoveDirection.STANDING) {
+          moveDirection = MoveDirection.DOWN;
+        }
+      }
+
+      return {
+        upQueue,
+        downQueue,
+        moveDirection
+      }
+
+    });
+
+    this.moveElevator();
+  }
+
+  render(): JSX.Element {
+    const floors = this.floorNumbers.map((number) =>
+      <button key={number.toString()}
+              onClick={() => this.handleClick(number)}
+              disabled={this.state.upQueue.has(number) || this.state.downQueue.has(number)}>
+        {number}
+      </button>
+    );
+
+    return (
+      <div className='main'>
+        <div>{this.state.currentFloor} {this.state.moveDirection}</div>
+        <div className='floor-btn'>
+          {floors}
+        </div>
+      </div>
+
+    );
+  }
 
 }
 
-export default App;
+export default Elevator;
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Elevator from "./App";
 
-/*ReactDOM.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Elevator />
   </React.StrictMode>,
   document.getElementById('root')
-);*/
-ReactDOM.render(
-    <h1>Hello, world!</h1>,
-    document.getElementById('root')
 );
+
 
 
 // If you want to start measuring performance in your app, pass a function
